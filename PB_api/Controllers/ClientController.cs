7e3b0e679bc65e2c48e8a78aa677ec3e65521853
Felipe Alves <falves99.dev@gmail.com>
2023-07-01 11:08:29 -0300

    [INIT] Initial project setup

---
+++ PB_api/Controllers/ClientController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PB_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController : ControllerBase
+    {
+        private readonly ILogger<ClientController> _logger;
+
+        public ClientController(ILogger<ClientController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
